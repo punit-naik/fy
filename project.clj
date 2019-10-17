b385c053a3e6a5cc84d63507a7bd9224edb0009a
Punit Naik <naik.punit44@gmail.com>
2019-10-17 17:10:40 +0530

    included image and i/o libs

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [clj-http "3.10.0"]
                  [metosin/reitit "0.3.10"]
-                 [ring/ring-jetty-adapter "1.7.1"]]
+                 [ring/ring-jetty-adapter "1.7.1"]
+                 [image-resizer "0.1.10"]
+                 [clj-commons/fs "1.5.1"]]
   :aot :all
   :main fy.core
   :erpl-option {:init-ns fy.core})
