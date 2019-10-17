b200785d46c3f94b5df6838c4099f3a01d614c45
Punit Naik <naik.punit44@gmail.com>
2019-10-17 17:13:45 +0530

    :fire:

--- src/fy/flickr_images.clj
+++
@@ -1,7 +0,0 @@
-
-(ns fy.flickr-images)
-
-(defn fetch
-  "Fetches flickr images from the URL: https://www.flickr.com/services/feeds/photos_public.gne"
-  []
-  (constantly {:status 200 :body "It works!"}))
